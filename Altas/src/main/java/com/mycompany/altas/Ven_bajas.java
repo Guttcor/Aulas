b38a6dc9faa3689e0d4f5e222bd2d15d0693fe65
Kevin <gutcor48@gmail.com>
2023-05-17 22:52:30 -0600

    Uní todos los proyectos al proyecto Altas, trabajaremos ahí

---
+++ Altas/src/main/java/com/mycompany/altas/Ven_bajas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.altas;
+
+/**
+ *
+ * @author Gael Mendoza
+ */
+public class Ven_bajas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
