148a7a1b4a7b95d105281ebf5b2730ed1035e2cc
Gael <mendoza030104@gmail.com>
2023-06-08 07:15:56 -0500

    Diswño sw intterfaces

---
+++ Desktop/proyecto bueno adp/Aulas/src/main/java/com/mycompany/altas/Administrador.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.altas;
+
+/**
+ *
+ * @author Jonathan Beristain
+ */
+public class Administrador {
+    
+    public static void main(String[] args){ 
+    Diagrama_de_casos objeto = new Diagrama_de_casos ("", "", "", "", "", "", "", "", "");
+        objeto.visualizar();
+    
+}
+}
